7233d3c2a456cbc212517bc4b466182af86aa582
githubfisher <gooodfisher@126.com>
2019-10-10 16:28:37 +0800

    添加UserLayout

---
+++ src/layouts/UserLayout/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid } from '@alifd/next';
+import Footer from './components/Footer';
+import styles from './index.module.scss';
+
+const { Row, Col } = Grid;
+
+export default function UserLayout(props) {
+  return (
+    <div className={styles.container}>
+      {props.children}
+      <Footer />
+    </div>
+  );
+}
